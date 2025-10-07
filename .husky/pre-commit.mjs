51371518a40cc5616ac126872caa1d6f230fcd56
Gabriel Gomazako <gabriel.f_gomazako@outlook.com>
2025-10-07 15:41:24 -0300

    feat: init hero component

--- .husky/pre-commit.mjs
+++ .husky/pre-commit.mjs
@@ -1,4 +1,8 @@
 import { exec } from "node:child_process";
+/**
+ * Esse arquivo é responsável por entrar em cada uma das definições dos blocks e mergea-los no component-models.json,
+ *  componente-definition.json e component-filters.json. Vide package-json
+ */
 
 const run = (cmd) => new Promise((resolve, reject) => exec(
   cmd,
