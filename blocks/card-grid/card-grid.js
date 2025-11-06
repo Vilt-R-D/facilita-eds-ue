dacb2590e78716690904cf4f1d6284238a87a9be
Enzo Amorim <enzo.amorim@vilt-group.com>
2025-11-06 10:49:04 -0300

    feat: add card grid component definition

---
+++ blocks/card-grid/card-grid.js
@@ -0,0 +1,7 @@
+/**
+ * loads and decorates the hero, mainly the nav
+ * @param {Element} block The hero block element
+ */
+export default async function decorate(block) {
+  console.log(block);
+}
