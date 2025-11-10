aae643b6b2ba820832c166972e26e630f658b977
Arthur Santos <arthur.santos@vilt.local>
2025-11-09 21:48:47 -0300

    debug(teaser): fix lint

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-  const el = document.createElement('h1')
-  el.textContent = 'vai corinthians'
-  block.append(el)
+  const el = document.createElement('h1');
+  el.textContent = 'vai corinthians';
+  block.append(el);
 }
