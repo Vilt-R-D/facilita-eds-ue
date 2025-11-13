79cb64e08c19468e028355349b4f233d45782b94
Enzo Amorim <enzo.amorim@vilt-group.com>
2025-11-13 11:17:14 -0300

    feat: add app-teaser component

---
+++ blocks/app-teaser/app-teaser.js
@@ -0,0 +1,8 @@
+
+/**
+ * loads and decorates the hero, mainly the nav
+ * @param {Element} block The hero block element
+ */
+export default async function decorate(block) {
+  console.log(block);
+}
